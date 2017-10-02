5603e963b4b4c076acd2ba8e69e11e0e6d8f6fb2
jo314652 <jo314652@gmail.com>
2017-10-02 21:25:03 +0800

    steven

---
+++ source/Main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	printf(" *********\t   ***   \t    *    \t    *    \n");
+	printf(" *       *\t *     * \t   ***   \t   * *   \n");
+	printf(" *       *\t*       *\t  *****  \t  *   *  \n");
+	printf(" *       *\t*       *\t    *    \t *     * \n");
+	printf(" *       *\t*       *\t    *    \t*       *\n");
+	printf(" *       *\t*       *\t    *    \t *     * \n");
+	printf(" *       *\t*       *\t    *    \t  *   *  \n");
+	printf(" *       *\t *     * \t    *    \t   * *   \n");
+	printf(" *********\t   ***   \t    *    \t    *    \n");
+
+	system("pause");
+	return 0;
+}
